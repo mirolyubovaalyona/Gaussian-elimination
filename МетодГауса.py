f31e1fe9bf603f3c83077537ba2860befa0bcda9
mirolyubovaalyona <niroliubova.aa@students.dvfu.ru>
2021-04-28 04:05:10 +1000

    Я сделала нормально ( Метод оптимального исключения)

--- МетодГауса.py
+++ МетодГауса.py
@@ -2,45 +2,64 @@ import numpy as np
 import sys
 
 
-n = int(input('Enter number of unknowns: '))
+n = int(input('Количество неизвестных: '))
 
-# Making numpy array of n x n+1 size and initializing 
-# to zero for storing augmented matrix
-a = np.zeros((n,n+1))
+a = np.zeros((n,n))
+b = np.zeros(n)
 
-# Making numpy array of n size and initializing 
-# to zero for storing solution vector
-x = np.zeros(n)
-
-# Reading augmented matrix coefficients
-print('Enter Augmented Matrix Coefficients:')
 for i in range(n):
-    for j in range(n+1):
+    for j in range(n):
         a[i][j] = float(input( 'a['+str(i)+']['+ str(j)+']='))
 
-# Applying Gauss Elimination
 for i in range(n):
-    if a[i][i] == 0.0:
-        sys.exit('Divide by zero detected!')
-        
-    for j in range(i+1, n):
-        ratio = a[j][i]/a[i][i]
-        
-        for k in range(n+1):
-            a[j][k] = a[j][k] - ratio * a[i][k]
+        b[i] = float(input( 'b['+str(i)+']='))
 
-# Back Substitution
-x[n-1] = a[n-1][n]/a[n-1][n-1]
 
-for i in range(n-2,-1,-1):
-    x[i] = a[i][n]
-    
-    for j in range(i+1,n):
-        x[i] = x[i] - a[i][j]*x[j]
-    
-    x[i] = x[i]/a[i][i]
+def f(n, a, b):
+    x = np.zeros(n)
+    m = np.zeros((n,n+1))
+    mm = np.zeros(n+1)
+
+    for i in range(n):
+        for j in range(n):
+            m[i][j]=a[i][j]
+        m[i][n]=b[i]
+
+    for i in range(n+1):
+        m[0][i]=m[0][i]/a[0][0]
+    print(m)
+
+    for k in range(1, n):
+        print(k)
+
+        for kk in range(k):
+            for i in range(n+1):
+                mm[i]=m[kk][i]*m[k][kk]
+            print("mm\n", mm)
+            for i in range(n+1):
+                m[k][i]-=mm[i]
+            print('вычитание предыдущих к\n', m)
+
+        q=m[k][k]
+        for j in range(n+1):
+            m[k][j]=m[k][j]/q
+            print('деление\n', m)
+
+        for kk in range(k):
+            for i in range(n+1):
+                mm[i]=m[kk][k]*m[k][i]
+            for i in range(n+1):
+                m[kk][i]-=mm[i]
+        print('vz', m)
+
+    for i in range(n):
+        x[i]=m[i][n]
+        
+    return x
+
+x=f(n, a, b)
 
 # Displaying solution
-print('\nRequired solution is: ')
+print('Теперь известные элементы: ')
 for i in range(n):
     print('X%d = %0.2f' %(i,x[i]), end = '\t')
